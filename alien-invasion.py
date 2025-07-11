3c03c02f77c1af71ab488af4dbbe535fb0f500d5
Zacky-njenga <zacharynjenga380@gmail.com>
2025-07-11 12:38:03 +0300

    added timer and color to the screen

--- alien-invasion.py
+++ alien-invasion.py
@@ -7,9 +7,13 @@ class  AlieInvasion:
     def __init__(self):
         """Initializes the game ad creates game resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
-        self.screen = pygame.display.set_mode((1500, 1000))
+        self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("ALIEN INVASION")
+
+        # set the background color
+        self.bg_color = (210, 235, 240)
 
     def run_game(self):
         """Starts the main loop for the game"""
@@ -18,9 +22,12 @@ class  AlieInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
 
             """Make the most recently drawn screen visible"""
             pygame.display.flip()
+            self.clock.tick(75)
 if __name__ == '__main__':
     """Make a game instance and run the game"""
 
